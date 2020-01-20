af3ad42be122a9814921539b40e17c985b6474fd
Zhehao Xie <zhehaox@uci.edu>
2020-01-19 17:42:32 -0800

    config setting

--- CarGame/main.cpp
+++ CarGame/main.cpp
@@ -11,11 +11,6 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE prevInstance,
 	UNREFERENCED_PARAMETER(showCmd);
 	//debug for memory allocation
 
-
-#if defined(DEBUG) | defined(_DEBUG)
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-#endif
-
 	CarGameApp theApp(hInstance);
 
 	return theApp.Run();
